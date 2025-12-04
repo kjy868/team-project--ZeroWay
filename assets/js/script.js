dedd0cdbe4f2b59901f5833b78f36f936f05699c
kjy868 <lkj109402@gmail.com>
2025-12-04 09:31:47 +0900

    12.04 header primary
    header nav에 해당되는 페이지에 primary color 적용하는 js, css 작성

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
+// header 해당 페이지 primary color 적용 ===================
+document.addEventListener('DOMContentLoaded', () => {
+    let current = window.location.pathname.split("/").pop();
 
+    if (current === "") current = "index.html";
+
+    const menus = document.querySelectorAll(".menu-list .menu a");
+
+    menus.forEach(menu => {
+        const link = menu.getAttribute("href");
+
+        if (link.includes(current)) {
+            menu.classList.add("active");
+        }
+    });
+});
 
 // GSAP ScrollTrigger 애니메이션 ==========================
 gsap.registerPlugin(ScrollTrigger);
